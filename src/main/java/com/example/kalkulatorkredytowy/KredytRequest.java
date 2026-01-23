a27b75336e39bd6951d6740c91fbe2780338e97f
Patryk Tomaszek <patryktomaszek6@gmail.com>
2026-01-23 14:37:57 +0100

    Adding Lombok to the request

--- src/main/java/com/example/kalkulatorkredytowy/KredytRequest.java
+++ src/main/java/com/example/kalkulatorkredytowy/KredytRequest.java
@@ -1,48 +1,17 @@
-package com.example.kalkulatorkredytowy;
+package com.example.kalkulatorfinansowy;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class KredytRequest {
-   private  BigDecimal amount;
-   private int months;
-   private LocalDate loanDate;
-   private BigDecimal annualInterestRate;
-
-   public KredytRequest(){}
-
-  public BigDecimal getAmount(){
-
-       return amount;
-  }
-  public void setAmount(BigDecimal amount ){
-
-       this.amount = amount;
-  }
-  public int getMonths(){
-
-       return months;
-  }
-  public void setMonths(int months) {
-
-       this.months = months;
-  }
-  public LocalDate getLoanDate(){
-
-       return loanDate;
-  }
-  public void setLoanDate(LocalDate loanDate){
-      this.loanDate = loanDate;
-  }
-  public BigDecimal getAnnualInterestRate(){
-
-       return annualInterestRate;
-  }
-  public void setAnnualInterestRate(BigDecimal annualInterestRate){
-
-       this.annualInterestRate = annualInterestRate;
-    }
-
-
-
-
+    private BigDecimal amount;
+    private int months;
+    private LocalDate loanDate;
+    private BigDecimal annualInterestRate;
 }
