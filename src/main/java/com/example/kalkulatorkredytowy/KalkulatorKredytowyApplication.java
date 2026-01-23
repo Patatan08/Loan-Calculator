dd676139fa7b4d53949ee5db1f3865f544bae695
Patryk Tomaszek <patryktomaszek6@gmail.com>
2026-01-23 14:07:00 +0100

    Loan Calculator

---
+++ src/main/java/com/example/kalkulatorkredytowy/KalkulatorKredytowyApplication.java
@@ -0,0 +1,13 @@
+package com.example.kalkulatorkredytowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalkulatorKredytowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KalkulatorKredytowyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kalkulatorkredytowy/KalkulatorKredytowyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kalkulatorkredytowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalkulatorKredytowyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
