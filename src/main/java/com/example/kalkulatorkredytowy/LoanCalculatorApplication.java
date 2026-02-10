175c9772a1ff34ad052b1a7ea82e2e2029fecf1c
Patryk Tomaszek <patryktomaszek6@gmail.com>
2026-02-10 10:29:33 +0100

    Name refactor

--- src/main/java/com/example/kalkulatorkredytowy/LoanCalculatorApplication.java
+++ src/main/java/com/example/kalkulatorkredytowy/LoanCalculatorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KalkulatorKredytowyApplication {
+public class LoanCalculatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KalkulatorKredytowyApplication.class, args);
+        SpringApplication.run(LoanCalculatorApplication.class, args);
     }
 
 }
